d74ac45b3b635fc3fc45776f3c9704048ff54ff4
Richard <rsulzenbacher@nomos-system.com>
2022-07-29 14:06:32 +0200

    Reverted back to jQuery

--- jquery.minicolors.js
+++ jquery.minicolors.js
@@ -8,10 +8,10 @@
 (function (factory) {
   if(typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['jquery-slim'], factory);
+    define(['jquery'], factory);
   } else if(typeof exports === 'object') {
     // Node/CommonJS
-    module.exports = factory(require('jquery-slim'));
+    module.exports = factory(require('jquery'));
   } else {
     // Browser globals
     factory(jQuery);
